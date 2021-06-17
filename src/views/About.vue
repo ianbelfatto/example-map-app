cee3fa8f08b389a4d134f5416e657d7b34cd919f
ianbelfatto <ibelfatto@gmail.com>
2021-06-17 11:24:19 -0400

    adds token, and css/js links, changes location to kingston, ny, adds pins in kingston and san diego

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="map">This is the map page!</div>
   </div>
 </template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
+<script>
+/* global mapboxgl */
+export default {
+  mounted: function () {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiaWJlbGZhdHRvIiwiYSI6ImNrcTExbnl2MjBhaXYyd2sxMnljeWc5aDgifQ.BAOYySyRiLY8iGwyugHqEw";
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-73.9973608, 41.9270367], // starting position [lng, lat]
+      zoom: 11, // starting zoom
+    });
+    new mapboxgl.Marker({ color: "green" }).setLngLat([-73.9973608, 41.9270367]).addTo(map);
+    new mapboxgl.Marker({ color: "blue" }).setLngLat([-117, 32.7]).addTo(map);
+  },
+};
+</script>
