7121a28623f11c3f6b93b41baa98894aa39e431e
ianbelfatto <ibelfatto@gmail.com>
2021-06-17 13:50:41 -0400

    adds places array and loop to create marker/popup

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,6 +15,15 @@
 <script>
 /* global mapboxgl */
 export default {
+  data: function () {
+    return {
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" },
+        { lat: 37.4221, lng: -122.078827, description: "Google HQ!" },
+      ],
+    };
+  },
   mounted: function () {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiaWJlbGZhdHRvIiwiYSI6ImNrcTExbnl2MjBhaXYyd2sxMnljeWc5aDgifQ.BAOYySyRiLY8iGwyugHqEw";
@@ -25,13 +34,24 @@ export default {
       zoom: 4, // starting zoom
     });
 
+    // Map Marker Loop Using 'places' Array
+
+    this.places.forEach((place) => {
+      var placeCoords = [place.lng, place.lat];
+      var placeDescription = place.description;
+      var descriptionPopup = new mapboxgl.Popup({ offset: 25 }).setText(placeDescription).addTo(map);
+      new mapboxgl.Marker({ color: "yellow" }).setLngLat(placeCoords).setPopup(descriptionPopup).addTo(map);
+    });
+
     // Map Popups
+
     var popup = new mapboxgl.Popup({ offset: 25 }).setText("My aunt lives here!").addTo(map);
     console.log(popup);
     var popup2 = new mapboxgl.Popup({ offset: 25 }).setText("I live here!").addTo(map);
     console.log(popup);
 
     // Map Markers
+
     new mapboxgl.Marker({ color: "green" }).setLngLat([-73.9973608, 41.9270367]).setPopup(popup2).addTo(map);
     // new mapboxgl.Marker({ color: "blue" }).setLngLat([-117, 32.7]).addTo(map);
     var marker2 = new mapboxgl.Marker({ color: "blue" }).setLngLat([-117, 32.7]).setPopup(popup).addTo(map);
